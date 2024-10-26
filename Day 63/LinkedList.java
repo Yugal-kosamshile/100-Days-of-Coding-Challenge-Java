2df1322a039e3fceab6b38c809afe1588df86539
Yugal Y. K. <140834062+Yugal-kosamshile@users.noreply.github.com>
2024-10-26 13:50:14 +0530

    Create LinkedList.java

---
+++ Day 63/LinkedList.java
@@ -0,0 +1,76 @@
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class LinkedList {
+    private Node head;
+
+    // Add a new node to the end of the list
+    public void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+        System.out.println("Adding " + data);
+    }
+
+    // Delete the first node with a given value
+    public void delete(int data) {
+        if (head == null) {
+            System.out.println("List is empty!");
+            return;
+        }
+        if (head.data == data) {
+            head = head.next;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null && temp.next.data != data) {
+            temp = temp.next;
+        }
+        if (temp.next == null) {
+            System.out.println("Element " + data + " not found!");
+        } else {
+            temp.next = temp.next.next;
+            System.out.println("Deleting " + data);
+        }
+    }
+
+    // Display the linked list
+    public void display() {
+        if (head == null) {
+            System.out.println("List is empty!");
+            return;
+        }
+        Node temp = head;
+        System.out.print("Linked List: ");
+        while (temp != null) {
+            System.out.print(temp.data + (temp.next != null ? " -> " : ""));
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.display();
+        list.delete(20);
+        list.display();
+    }
+}
